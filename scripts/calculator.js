68f98d62f8d7dfa78b606173d444461d1a5f1545
Anshul Sambher <anshulsambher@Anshuls-MacBook-Air.local>
2025-06-17 13:10:45 +0530

    Improved the script for better display and removed some display bugs

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -19,28 +19,42 @@ function updateCalculation(buttonVal) {
     calculation = calculation.replace('\u2212', '-');
     calculation = calculation.replace('\u00D7', '*');
     calculation = calculation.replace('\u00F7', '/');
-    calculation = String(eval(calculation).toFixed(10));
+    calculation = String(eval(calculation));
     equated = true;
   } else if (buttonVal === 'Clear') {
     calculation = '0';
     equated = false;
   } else if (buttonVal === 'Cut') {
-    calculation = calculation.slice(0, -1);
+    if (calculation.at(-1) === ' ') {
+      calculation = calculation.slice(0, -3);
+    } else {
+      calculation = calculation.slice(0, -1);
+    }
     if (calculation === '') {
       calculation = '0';
     }
     equated = false;
   } else {
+    if (calculation.at(-1) === ' ') {
+      calculation = calculation.slice(0, -3);
+    }
     calculation += ` ${buttonVal} `;
     equated = false;
   }
 
   localStorage.setItem('calculation', calculation);
-  displayCalculation();
+  displayCalculation(buttonVal);
 }
 
-function displayCalculation() {
+function displayCalculation(buttonVal = '=') {
+  console.log(buttonVal);
+  console.log(calculation);
+  console.log(calculation.at(-1));
   screen = document.querySelector('.results-tab');
   screen.innerHTML = calculation;
-  screen.scrollLeft = screen.scrollWidth;
+  if (buttonVal === '=') {
+    screen.scrollLeft = 0;
+  } else {
+    screen.scrollLeft = screen.scrollWidth;
+  }
 }
